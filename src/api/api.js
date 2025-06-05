ca77357c7c7c961120807eb2fb3be875ac859adc
kudratov26 <qudratov.com26@gmail.com>
2025-06-05 22:58:20 +0500

    done

---
+++ src/api/api.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://o-complex.com:1337/',
+});
+
+export const getReviews = async () => {
+    try {
+        const response = await api.get('/reviews');
+        return response.data;
+    } catch (error) {
+        console.error('Ошибка при получении отзывов:', error);
+        throw error;
+    }
+};
+export const getProducts = async (page = 1, pageSize = 20) => {
+    try {
+        const response = await api.get('/products', {
+            params: { page, page_size: pageSize },
+        });
+
+        return {
+            products: response.data.items || [],
+            total: response.data.total || 0
+        };
+    } catch (error) {
+        console.error('Ошибка при получении товаров:', error);
+        return { products: [], total: 0 }; 
+    }
+};
+
+
+export const createOrder = async (order) => {
+    const finalOrder = JSON.stringify(order)
+    try {
+        const response = await api.post('/order', finalOrder, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Ошибка при создании заказа:', error);
+        throw error;
+    }
+};
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,60 @@
+"use client"
+import Image from "next/image";
+import { FormCounter } from "@/components/FormCounter/FormCounter";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/Button/Button";
+import { useCart } from "@/contexts/CartContext";
+
+export const Card = ({ product }) => {
+    if (!product) return null; // 💥 Защита от undefined/null
+    const { description, id, image_url, price, title } = product
+
+    const { cartItems, addToCart, removeFromCart, customerPhone = '', updateCustomerPhone, resetContext } = useCart();
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(prevCount => prevCount + 1);
+    };
+
+    const decrement = () => {
+        setCount(prevCount => prevCount - 1);
+    };
+
+    useEffect(() => {
+        if (count > 0) {
+            addToCart(product, count);
+        } else {
+            removeFromCart(product.id);
+        }
+    }, [count]);
+
+    useEffect(() => {
+        console.log('count', count)
+    }, [count]);
+
+    useEffect(() => {
+        if (cartItems.length === 0) {
+            setCount(cartItems.length)
+        }
+    }, [cartItems])
+
+    return (
+        <div className='flex flex-col justify-start items-center bg-[#D9D9D9] rounded-2xl w-full max-w-sm sm:max-w-[301px] px-2.5 py-2 gap-2'>
+            <Image
+                src={image_url}
+                alt="Описание изображения"
+                width={309}
+                height={366}
+                unoptimized={true}
+                className="rounded-2xl object-center object-contain w-full"
+            />
+            <h2 className='text-black text-4xl word-wrap overflow text-overflow white-space title text-left'>{title}</h2>
+            <p className='w-full text-black mb-auto desc'>{description}</p>
+            <span className='text-black text-2xl mt-auto'>{`ценa: ${price}₽`}</span>
+            {count >= 1 ?
+                <FormCounter setCount={setCount} count={count} increment={increment} decrement={decrement} /> :
+                <Button onClick={increment} size={'full'} title={'купить'} />}
+        </div>
+    )
+}
